e77b13adab5c2552be4291dc161f31f3f2f6c09f
Alexandr Kvashin <kvashin.am98@gmail.com>
2020-08-10 01:28:50 +0700

    move MainPage to /Views. add CurrencySelectionPage

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace currency_converter.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Preventing non-digit input
+        // The task text says that only standard controls should be used. Otherwise, it is possible to use NumberBox
+        // https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/number-box
+        private void TextBox_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            if (args.NewText == string.Empty)
+            {
+                sender.Text = string.Empty;
+            }
+            else
+            {
+                bool isDouble = double.TryParse(args.NewText, out _);
+                bool isNegative = args.NewText.Any(c => c == '-');
+                args.Cancel = !isDouble || isNegative;
+            }
+        }
+    }
+}
